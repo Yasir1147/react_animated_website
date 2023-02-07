3dc8edec3c51c0ec905a60b156811cb882c6f31d
signum pc <developer.bohr@gmail.com>
2023-02-07 17:44:40 +0500

    Updates

---
+++ precache-manifest.7a2069225ddb92cc2564fdfeb6cab314.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec691483a2fcc3bd5195",
+    "url": "/static/css/main.4d4abba2.chunk.css"
+  },
+  {
+    "revision": "ec691483a2fcc3bd5195",
+    "url": "/static/js/main.18aae3b4.chunk.js"
+  },
+  {
+    "revision": "ad834586ed3382ddf2d7",
+    "url": "/static/js/runtime~main.bf02eaaa.js"
+  },
+  {
+    "revision": "06201203e4a0c26d1e12",
+    "url": "/static/css/2.25f83752.chunk.css"
+  },
+  {
+    "revision": "06201203e4a0c26d1e12",
+    "url": "/static/js/2.ddd405e0.chunk.js"
+  },
+  {
+    "revision": "4af07b562c0602e03473",
+    "url": "/static/js/3.a772fac8.chunk.js"
+  },
+  {
+    "revision": "891595fe558d2b964d88a83bab5656be",
+    "url": "/static/media/contact_bk.891595fe.jpg"
+  },
+  {
+    "revision": "c3248e94a6bf33c7d8996e5761fe643c",
+    "url": "/static/media/home_back.c3248e94.jpg"
+  },
+  {
+    "revision": "370d88e0705a6cbbf7d4352acf0a0b7d",
+    "url": "/index.html"
+  }
+];
